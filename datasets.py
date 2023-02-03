9667fd3a65f48000a7cedc8c6049ec4a0656a65b
unknown <shindesanchit33@gmail.com>
2023-02-03 23:14:25 +0530

    Created class VITONDataset

--- datasets.py
+++ datasets.py
@@ -1,2 +1,31 @@
+import numpy as np
+import torch
+
+
+
+class VITONDataset(data.Dataset):
+    def __init__(self, opt):
+        super(VITONDataset, self).__init__()
+        self.load_height = opt.load_height
+        self.load_width = opt.load_width
+        self.semantic_nc = opt.semantic_nc
+        self.data_path = osp.join(opt.dataset_dir, opt.dataset_mode)
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        # load data list
+        img_names = []
+        c_names = []
+        with open(osp.join(opt.dataset_dir, opt.dataset_list), 'r') as f:
+            for line in f.readlines():
+                img_name, c_name = line.strip().split()
+                img_names.append(img_name)
+                c_names.append(c_name)
+
+        self.img_names = img_names
+        self.c_names = dict()
+        self.c_names['unpaired'] = c_names
 
     
