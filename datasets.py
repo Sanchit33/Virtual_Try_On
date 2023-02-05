67bf7b6c9f34cd44be324bab5d7f9cc2b561157e
unknown <shindesanchit33@gmail.com>
2023-02-05 18:47:06 +0530

    created get parse agnostic

--- datasets.py
+++ datasets.py
@@ -28,4 +28,30 @@ class VITONDataset(data.Dataset):
         self.c_names = dict()
         self.c_names['unpaired'] = c_names
 
+    def get_parse_agnostic(self, parse, pose_data):
+        parse_array = np.array(parse)
+        parse_upper = ((parse_array == 5).astype(np.float32) +
+                       (parse_array == 6).astype(np.float32) +
+                       (parse_array == 7).astype(np.float32))
+        parse_neck = (parse_array == 10).astype(np.float32)
+
+        r = 10
+        agnostic = parse.copy()
+
+        # mask arms
+        for parse_id, pose_ids in [(14, [2, 5, 6, 7]), (15, [5, 2, 3, 4])]:
+            mask_arm = Image.new('L', (self.load_width, self.load_height), 'black')
+            mask_arm_draw = ImageDraw.Draw(mask_arm)
+            i_prev = pose_ids[0]
+            for i in pose_ids[1:]:
+                if (pose_data[i_prev, 0] == 0.0 and pose_data[i_prev, 1] == 0.0) or (pose_data[i, 0] == 0.0 and pose_data[i, 1] == 0.0):
+                    continue
+                mask_arm_draw.line([tuple(pose_data[j]) for j in [i_prev, i]], 'white', width=r*10)
+                pointx, pointy = pose_data[i]
+                radius = r*4 if i == pose_ids[-1] else r*15
+                mask_arm_draw.ellipse((pointx-radius, pointy-radius, pointx+radius, pointy+radius), 'white', 'white')
+                i_prev = i
+            parse_arm = (np.array(mask_arm) / 255) * (parse_array == parse_id).astype(np.float32)
+            agnostic.paste(0, None, Image.fromarray(np.uint8(parse_arm * 255), 'L'))
+
     
